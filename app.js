bc23ee55702eb012bed394b0f6ad678944ab5cfa
archita172020 <62419809+archita172020@users.noreply.github.com>
2022-03-12 12:32:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    "<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Welcome to the page</p></h1></body></html>"
+  );
+});
